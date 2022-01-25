53fc5ea6f643b3858f96c2f0f805c626d4ccce4a
DanielSantamariaAlvarez <47115911+DanielSantamariaAlvarez@users.noreply.github.com>
2022-01-25 15:49:28 -0500

    creación del Json con los usuarios

--- app.py
+++ app.py
@@ -1,32 +1,83 @@
 import requests
 import pandas as pd
 import haversine as hav
+import numba
+import json
 
-def callUsers():
-    response = requests.get(API)
+@numba.jit
+def callUsers(users):
+    response = requests.get("https://sccr8pgns0.execute-api.us-east-1.amazonaws.com/dev/locations")
     if response.status_code == 200:
-        users = []
-        for user_id in range(5):
+        for user_id in range(10000):
             llamado = requests.get('https://sccr8pgns0.execute-api.us-east-1.amazonaws.com/dev/locations/'+str(user_id)).json()["data"]
-            print(llamado)
-            users = llamado
+            users.append(llamado)
     else:
         print("error en la api")
+    print(users)
+
+def createUsersInJson():
+    # Tiempo aproximado de hora a hora y media para traer los 10000 usuarios
+    # con mi internet de 6 megas
+    users=[]
+    callUsers(users)
+    with open('users.json','w') as f:
+        json.dump(users, f, indent=4)
 
 def createAirportsCsv():
     data = pd.read_csv('https://davidmegginson.github.io/ourairports-data/airports.csv')
     dataW = data[data["wikipedia_link"].notnull()]
     dataW.to_csv("airports_w_wiki.csv", encoding='utf-8', index=False)
 
+def loadUsers():
+    users = None
+    with open('users.json') as f:
+        users = json.load(f)
+    return users
+
+@numba.jit
+def createJsonAirport():
+    json1 = []
+    users = loadUsers()
+    data = pd.read_csv('airports_w_wiki.csv')
+
+    for i in users:
+        latitud1 = float(i['latitude']['N'])
+        longitud1 = float(i['longitude']['N'])
+        id = int(i['user_id']['N'])
+        max = 0
+        airport = ''
+
+        for _, fila in data.iterrows():
+            j = fila.to_dict()
+            latitud2 = float(j['latitude_deg'])
+            longitud2 = float(j['longitude_deg'])
+            distancia = hav.haversine((longitud1), (latitud1), (longitud2), (latitud2))
+            if distancia > max:
+                max = distancia
+                airport = j
+        newDict = {'user_id':id, 'latitude':latitud1, 'longitude':longitud1}
+        newDict.update({'distance':max})
+        newDict.update(airport)
+        finalDict = {id:newDict}
+        json1.append(finalDict)
+        
+    return json
+
+
+    
+
+
 if __name__ == '__main__':
     API_AIRPORTS = "https://davidmegginson.github.io/ourairports-data/airports.csv"
     API_USERS = "https://sccr8pgns0.execute-api.us-east-1.amazonaws.com/dev/locations"
-    lon1 = -103.548851
-    lat1 = 32.0004311
-    lon2 = -103.6041946
-    lat2 = 33.374939
 
-    print(hav.haversine(lat1, lon1, lat2, lon2))
+    users = createJsonAirport()
+    with open('airports.json', 'w') as f:
+        json.dump(users, f, indent=4)
+
+
+
+    
 
 
     
